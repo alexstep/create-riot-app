567d3fa3d81a38c84684635f6589b21b307b7eee
AlexStep <alex.step.kellas@gmail.com>
2017-07-18 18:08:07 +0200

    add .tag ext in dev webpack config

--- scripts/config/webpack.config.dev.js
+++ scripts/config/webpack.config.dev.js
@@ -111,7 +111,7 @@ let webpack_dev_config = {
 		// We also include JSX as a common component filename extension to support
 		// some tools, although we do not recommend using it, see:
 		// https://github.com/facebookincubator/create-react-app/issues/290
-		extensions: ['.js', '.json', '.jsx'],
+		extensions: ['.js', '.json', '.tag'],
 		alias: {
 		},
 		plugins: [
